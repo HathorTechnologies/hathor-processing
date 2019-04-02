a0447eaa4cd1399f075489a6aaca83a0d0898604
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-04-02 11:11:10 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='hathor-processing',
-    version='',
+    version='1.0.0.dev1',
     packages=['hathorprocessing'],
-    url='',
+    url='https://github.com/HathorTechnologies/hathor-processing',
     license='',
     author='',
     author_email='',
     description='',
+    python_requires='>=3',
     install_requires=[
         'pandas',
         'sqlalchemy'
