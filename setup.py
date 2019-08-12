95c9ce059eb99cb179bf5b44174936a75386d033
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-08-12 15:28:03 +0300

    Bump version: 1.2.7.dev → 1.2.8.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hathor-processing',
-    version='1.2.7.dev',
+    version='1.2.8.dev',
     packages=['hathorprocessing'],
     url='https://github.com/HathorTechnologies/hathor-processing',
     license='',
